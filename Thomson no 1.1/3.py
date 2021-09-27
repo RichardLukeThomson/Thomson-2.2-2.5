276f76e4a8080c321ed94b9fb1085993ec3072a3
Richard Thomson <richardlukethomson@outlook.com>
2021-09-27 08:18:35 +0100

    test

--- Thomson no 1.1/3.py
+++ Thomson no 1.1/3.py
@@ -156,3 +156,27 @@ if c0 > 1:
 	print("steps =",steps)
 else:
 	print("Bad c0 value")
+
+#3.4.1.6
+hat_list = [1, 2, 3, 4, 5]
+
+# Step 1
+hat_list[2] = int(input("Enter an integer number: "))
+
+# Step 2
+del hat_list[-1]
+
+# Step 3
+print(len(hat_list))
+
+#3.6.1.9
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+new_list = []
+for number in my_list:  # Browse all numbers from the source list.
+	if number not in new_list:  # If the number doesn't appear within the new list...
+		new_list.append(number)  # ...append it here.
+my_list = new_list[:]  # Make a copy of new_list.
+print("The list with unique elements only:")
+print(my_list)
+
+
